d78d8ce70b896b797530f7a695a180468a09b6ea
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-06 21:41:44 +0530

    Create TraineeDetails.java
    to print trainee details

---
+++ TraineeDetails.java
@@ -0,0 +1,7 @@
+public class TraineeDetails {
+	public static void main(String[] args) {
+		System.out.println("My name is Maria Jerome.");
+		System.out.println("My designation is Systems Engineer.");
+	}
+
+}
