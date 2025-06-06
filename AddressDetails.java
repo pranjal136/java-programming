3c76465e446465f177032971cc38f9241f9d93be
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-06 21:43:03 +0530

    Create AddressDetails.java
    to print person address

---
+++ AddressDetails.java
@@ -0,0 +1,9 @@
+public class AddressDetails {
+	public static void main(String[] args) {
+		System.out.println("Door No: D089");
+		System.out.println("Street: St. Louis Strret");
+		System.out.println("City: Springfield");
+		System.out.println("ZIP Code: 62729");
+	}
+
+}
