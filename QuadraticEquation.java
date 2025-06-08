78b3c5e8bdd58a69fc3fb8b032b2830554dc6f0a
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-08 18:46:57 +0530

    Create QuadraticEquation.java
    A program to solve quadratic equation and finding the discriminant value using the formula discriminant = b2 - 4ac

---
+++ QuadraticEquation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter c: ");
+        double c = input.nextDouble();
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Root = " + root);
+        } else {
+            System.out.println("The equation has no real root");
+        }
+        input.close();
+    }
+}
