6a00a32ce9db711d84b1204ab26c251a86d01dbe
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-13 17:57:44 +0530

    Create AdjacentOccurnceNum.java
    Find and return the count of the adjacent occurrence of a number in the numbers array

---
+++ AdjacentOccurnceNum.java
@@ -0,0 +1,22 @@
+/*
+Find and return the count of the adjacent occurrence of a number in the numbers array passed to the findTotalCount() method. Implement the logic inside findTotalCount() method.
+
+Test the functionalities using the main method of the Tester class.
+*/
+
+class Tester {
+	public static int findTotalCount(int[] numbers) {
+		int count = 0;
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] == numbers[i - 1]) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 1, 5, 100, -20, 6, 0, 0 };
+		System.out.println("Count of adjacent occurrence: " + findTotalCount(numbers));
+	}
+}
