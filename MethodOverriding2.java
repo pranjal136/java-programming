a8b69c0a702a192deeb845032bbc23cb4ef233c2
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-15 22:40:37 +0530

    Create MethodOverriding2.java
    example of method overriding and the usage of access specifiers in overridden methods.

---
+++ MethodOverriding2.java
@@ -0,0 +1,49 @@
+/*
+ * The code given below shows an example of method overriding and the usage of 
+ * access specifiers in overridden methods.
+Execute the code given below and observe the output.
+ */
+
+public class Tester2 {
+		public static void main(String args[]) {
+			Bank bank;
+			bank = new ABCBank();
+			System.out.println("ABCBank - Rate of Interest(%): "
+					+ bank.getRateOfInterest());
+			bank = new DEFBank();
+			System.out.println("DEFBank - Rate of Interest(%): "
+					+ bank.getRateOfInterest());
+			bank = new GHIBank();
+			System.out.println("GHIBank - Rate of Interest(%): "
+					+ bank.getRateOfInterest());
+		}
+	}
+
+	class Bank {
+		public float getRateOfInterest() {
+			return 0;
+		}
+		// remove the access specifier and observe the output
+	}
+
+	class ABCBank extends Bank {
+		public float getRateOfInterest() {
+			return 8.99f;
+		}
+		// Keeping the parent access specifier as public, remove the child access
+		// specifier and observe the output
+	}
+
+	class DEFBank extends Bank {
+		public float getRateOfInterest() {
+			return 9.4f;
+		}
+		// Keeping the parent access specifier as public, change the child access
+		// specifier to private/protected and observe the output
+	}
+
+	class GHIBank extends Bank {
+		public float getRateOfInterest() {
+			return 8.1f;
+		}
+}
