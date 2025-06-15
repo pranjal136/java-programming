ce78e7b545375f62c79c09a82b547d9447e4ee7b
Pranjal Agrawal <107926588+pranjal136@users.noreply.github.com>
2025-06-15 22:37:30 +0530

    Create MethodOverriding.java
    different possibilities of using parent and child class references and objects together

---
+++ MethodOverriding.java
@@ -0,0 +1,40 @@
+/*
+The code given below shows the different possibilities of using parent and child class references and objects together.
+Execute the code given below and observe the output.
+*/
+class Tester {
+
+	public static void main(String[] args) {
+
+		// Customer customer = new Customer();
+		// System.out.println("Final bill amount: "+customer.payBill(40.0));
+		// Parent Reference -> Parent Object
+
+		// RegularCustomer regularCustomer = new RegularCustomer();
+		// System.out.println("Final bill amount: "+regularCustomer.payBill(40.0));
+		// Child Reference -> Child Object
+
+		Customer regCust = new RegularCustomer();
+		// Parent Reference -> Child Object
+		System.out.println("Final Bill : " + regCust.payBill(40.0)); 
+	}
+}
+
+class Customer {
+
+	public double payBill(double totalPrice) {
+		System.out.println("Final bill for the customer is calculated here");
+		return totalPrice;
+	}
+}
+
+class RegularCustomer extends Customer {
+
+	@Override
+	public double payBill(double totalPrice) {
+		System.out.println("Final bill for the regular customer is calculated here");
+		double priceAfterDiscount = totalPrice * (1 - (5f / 100));
+		return priceAfterDiscount;
+	}
+
+}
